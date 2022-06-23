ff94328acc96de2d08e6a8809f4cf454f5cb914d
d0oda <fwang@perse.co.uk>
2022-06-23 14:36:10 +0100

    testing

---
+++ Design/UI.py
@@ -0,0 +1,4 @@
+def getStatistics():
+    pass
+
+def 
---
+++ Game.py
@@ -0,0 +1,9 @@
+class Game():
+  def __init__(self, player1, player2):
+    self.__player1 = Player(player1)
+    self.__player2 = Player(player2)
+  
+  
+
+class Player():
+  pass
