6b02ca9f2a6d2c1d818d3b087c7bb119344b5fbf
d0oda <fwang@perse.co.uk>
2022-06-23 14:59:14 +0100

    created board

--- Game.py
+++ Game.py
@@ -1,9 +1,20 @@
 class Game():
+
+  EMPTY = " "
+
   def __init__(self, player1, player2):
-    self.__player1 = Player(player1)
-    self.__player2 = Player(player2)
-  
-  
+    self.__player1 = Player()
+    self.__player2 = Player()
+    self.__board = []
+
+  def setupBoard(self):
+    for count in range(0, 6):
+      row = [Game.EMPTY] * 7
+      self.__board.append(row)
+    '''print(*self.__board, sep = "\n")'''
 
 class Player():
   pass
+
+game1 = Game("1", "2")
+game1.setupBoard()
