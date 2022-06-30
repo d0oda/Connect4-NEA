7013d74733785f3b50d0575d5cff8b3093730ef6
d0oda <fwang@perse.co.uk>
2022-06-30 08:57:31 +0100

    checkwin condition fix and column number displayed

--- Game.py
+++ Game.py
@@ -31,35 +31,33 @@ class Game():
       self.__board.append(row)
 
   def displayBoard(self):
-    for i in range(Game.HEIGHT):
-      print(*self.__board[i], sep = " ")
+    for i in range(1, Game.WIDTH + 1, 1):
+      print(i, end = " ")
+    print(" ")
+    for j in range(Game.HEIGHT):
+      print(*self.__board[j], sep = " ")
       print(" ")
 
-  def makeMove(self, column):
+  def placeMove(self, column):
     column -= 1
-    if self.validateMove(column) == True:
-        for row in range(Game.HEIGHT - 1, -1, -1):
-          if self.__board[row][column] == ".":
-            self.__board[row][column] = "R" if self.__playerTurn == 1 else "Y"
-            self.turnCounter()
-            self.displayBoard()
-            return
+    if self.validateMove(column):
+      for row in range(Game.HEIGHT - 1, -1, -1):
+        if self.__board[row][column] == ".":
+          self.__board[row][column] = "R" if self.__playerTurn == 1 else "Y"
+          return
     print(Fore.RED + "Move invalid")
-    return
 
   def validateMove(self, column):
-    if column >= 0 and column < 7:
-      if self.__board[0][column] != ".":
-        return False
-      return True
-    return False
+    return column >= 0 and column < Game.WIDTH and self.__board[0][column] == "."
 
   def turnCounter(self):
     self.__playerTurn = 3 - self.__playerTurn
+    
+  def displayTurn(self):
     if self.__playerTurn == 1:
-      print(Back.RED + "Player 1's Turn: ")
+      print(Back.RED + "Player 1's Turn ")
     else:
-      print(Back.YELLOW + "Player 2's Turn: ")
+      print(Back.YELLOW + "Player 2's Turn ")
     return self.__playerTurn
 
   def checkWin(self):
@@ -68,25 +66,23 @@ class Game():
       for column in range(Game.WIDTH - 3):
         if self.__board[row][column] == piece and self.__board[row][column + 1] == piece and self.__board[row][column + 2] == piece and self.__board[row][column + 3] == piece:
           return True
-      return False
 
     for column in range(Game.WIDTH):
       for row in range(Game.HEIGHT - 3):
         if self.__board[row][column] == piece and self.__board[row + 1][column] == piece and self.__board[row + 2][column] == piece and self.__board[row + 3][column] == piece:
           return True
-      return False
 
     for column in range(Game.WIDTH - 3):
       for row in range(Game.HEIGHT - 3):
         if self.__board[row][column] == piece and self.__board[row + 1][column + 1] == piece and self.__board[row + 2][column + 2] == piece and self.__board[row + 3][column + 3] == piece:
           return True
-      return False
 
     for column in range(Game.WIDTH - 3):
       for row in range(3, Game.HEIGHT):
         if self.__board[row][column] == piece and self.__board[row - 1][column + 1] == piece and self.__board[row - 2][column + 2] == piece and self.__board[row - 3][column + 3] == piece:
           return True
-      return False
+
+    return False
 
   def validateBoard(self):
     red = 0
@@ -111,9 +107,13 @@ class Player():
 if __name__ == "__main__":
   game = Game()
   game.gamemodeOption()
+  game.displayBoard()
   while True:
+    game.displayTurn()
     move = int(input("Make a move: "))
-    game.makeMove(move)
+    game.placeMove(move)
+    game.displayBoard()
     if game.checkWin():
       print("The winner is:")
       break
+    game.turnCounter()
