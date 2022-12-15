2c51fd8345534caa38c5c8fde2ac00e8271577b0
d0oda <fwang@perse.co.uk>
2022-12-15 16:38:29 +0000

    made queries more readable and added insertUser

--- Database.py
+++ Database.py
@@ -10,12 +10,37 @@ c.execute("CREATE TABLE SavedGames (gameID int PRIMARY KEY, player1 VARCHAR(50),
 class DBMS():
   def __init__(self):
     self.conn = sqlite3.connect("Connect4.db")
+
+
   def Createtables(self):
     c = self.conn.cursor()
-    c.execute("CREATE TABLE Users (username VARCHAR(50) PRIMARY KEY, password VARCHAR(50))")
-    c.execute("CREATE TABLE Statistics (statsID int PRIMARY KEY, username VARCHAR(50), numWins int, numLosses int, maxWinStreak int)")
-    c.execute("CREATE TABLE SavedGames (gameID int PRIMARY KEY, player1 VARCHAR(50), player2 VARCHAR(50), gameState)")
+    c.execute('''CREATE TABLE Users
+         (username VARCHAR(50) PRIMARY KEY NOT NULL,
+         password VARCHAR(50) NOT NULL);''')
+
+    c.execute('''CREATE TABLE Statistics
+         (statsID INT PRIMARY KEY NOT NULL,
+         username VARCHAR(50) NOT NULL,
+         numWins VARCHAR(50) NOT NULL,
+         numLosses VARCHAR(50) NOT NULL,
+         maxWinStreak INT NOT NULL);''')
+
+    c.execute('''CREATE TABLE SavedGames
+         (gameID INT PRIMARY KEY NOT NULL,
+         player1 VARCHAR(50) NOT NULL,
+         player2 VARCHAR(50) NOT NULL,
+         gameState NOT NULL);''')
+
     c.close()
+
+
   def InsertUser(self, username, password):
     c = self.conn.cursor()
-    c.execute("INSERT INTO Users")
+    c.execute("""INSERT INTO Users 
+        (username, password) 
+        VALUES (?, ?);""")
+
+    c.close()
+
+  def UpdateStats(self):
+    pass
